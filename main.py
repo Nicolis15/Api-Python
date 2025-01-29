7cc954c73993b5918b8de00d8dc7ec7e2a75a1cf
Nicolis15 <Nicolistrabajo@gmail.com>
2025-01-29 16:20:59 -0500

    Segundo commit

--- main.py
+++ main.py
@@ -7,3 +7,6 @@ app = FastAPI()
 def hola_mundo():
     return{"hola : mundo"}
 
+@app.get('/primerget/{id_usuario}')
+def primer_get(id: int, valor: Union[str,None]= None):
+    return{"id":id, "valor": valor}
